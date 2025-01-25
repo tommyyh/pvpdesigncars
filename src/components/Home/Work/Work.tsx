632456046d582c55d49b1bc3c24ee41f9bd139c8
Tommy Hoang <tommyyhoangg@gmail.com>
2025-01-25 21:10:34 +0100

    PHotos

--- src/components/Home/Work/Work.tsx
+++ src/components/Home/Work/Work.tsx
@@ -9,6 +9,15 @@ import Work3Png from '@/public/work/3.png';
 import Work4Png from '@/public/work/4.png';
 import Work5Png from '@/public/work/5.png';
 import Work6Png from '@/public/work/6.png';
+import Work7Png from '@/public/work/7.png';
+import Work8Png from '@/public/work/8.png';
+import Work9Png from '@/public/work/9.png';
+import Work10Png from '@/public/work/10.png';
+import Work11Png from '@/public/work/11.png';
+import Work12Png from '@/public/work/12.png';
+import Work13Png from '@/public/work/13.png';
+import Work14Png from '@/public/work/14.png';
+import Work15Png from '@/public/work/15.png';
 
 const Work = () => {
   return (
@@ -38,6 +47,42 @@ const Work = () => {
 
         <div className={style.img}>
           <ImageComponent src={Work6Png} alt="Ukázka práce" />
+        </div>
+
+        <div className={style.img}>
+          <ImageComponent src={Work7Png} alt="Ukázka práce" />
+        </div>
+
+        <div className={style.img}>
+          <ImageComponent src={Work8Png} alt="Ukázka práce" />
+        </div>
+
+        <div className={style.img}>
+          <ImageComponent src={Work9Png} alt="Ukázka práce" />
+        </div>
+
+        <div className={style.img}>
+          <ImageComponent src={Work10Png} alt="Ukázka práce" />
+        </div>
+
+        <div className={style.img}>
+          <ImageComponent src={Work11Png} alt="Ukázka práce" />
+        </div>
+
+        <div className={style.img}>
+          <ImageComponent src={Work12Png} alt="Ukázka práce" />
+        </div>
+
+        <div className={style.img}>
+          <ImageComponent src={Work13Png} alt="Ukázka práce" />
+        </div>
+
+        <div className={style.img}>
+          <ImageComponent src={Work14Png} alt="Ukázka práce" />
+        </div>
+
+        <div className={style.img}>
+          <ImageComponent src={Work15Png} alt="Ukázka práce" />
         </div>
       </div>
 
